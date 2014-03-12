05d91433a80ad82ff4646c0c5a101fbc5e04d48e
EpicGamexz <epicgamexz@gmail.com>
2014-03-11 18:34:04 -0600

    Update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forum::Application.config.secret_key_base = '9781866f9a8bc99724f0428b570fce98c46977b6763a72cbc5e70c57109ad53ef7aac8f075eebc5f2e4477e4b42506d56aa7e1c6ca2de422211a9b6e19af7366'
